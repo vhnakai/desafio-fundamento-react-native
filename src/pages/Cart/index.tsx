0c77760678333af56fa853beba6541c909a79688
vhnakai <hugo.nakai@gmail.com>
2020-07-24 10:20:17 -0300

    Desafio finalizado

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -47,15 +47,23 @@ const Cart: React.FC = () => {
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const productSubtotal = product.price * product.quantity;
 
-    return formatValue(0);
+      return accumulator + productSubtotal;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const productQuantity = product.quantity;
 
-    return 0;
+      return accumulator + productQuantity;
+    }, 0);
+
+    return total;
   }, [products]);
 
   return (
